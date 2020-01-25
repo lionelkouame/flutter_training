947d6e0ba83a13b994cb2fe1bda4c5c04c217dfb
Lionel Kouame <lionelkouame@gmail.com>
2020-01-25 17:07:43 +0100

    [#8] - init project pop.

---
+++ lib/app/popNavigator/body/index.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class BodyIndex extends StatefulWidget {
+  @override
+  _BodyIndexState createState() => new _BodyIndexState();
+}
+
+class _BodyIndexState extends State<BodyIndex> {
+  var btnValue = "";
+  var btnStatus = false;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Center(
+      child: RaisedButton(
+        onPressed: pressed,
+        child: new Text(btnValue),
+      ),
+    );
+  }
+
+  void pressed() {
+    setState(() {
+      btnStatus = !btnStatus;
+      (btnStatus) ? btnValue = "Btn is clicked" : btnValue = "clicked !!";
+    });
+  }
+}
