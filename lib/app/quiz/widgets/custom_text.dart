86e3f02070182526e3ebc5ef86dc53c688cd8284
Lionel Kouame <lionelkouame@gmail.com>
2020-01-27 23:16:28 +0100

    [#10] -  Quiz - Homepage quiz.

---
+++ lib/app/quiz/widgets/custom_text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomText extends Text {
+  CustomText(String data,
+      {color: Colors.black, textAlign: TextAlign.center, factor: 1.0})
+      : super(data,
+            textAlign: textAlign,
+            textScaleFactor: factor,
+            style: new TextStyle(color: color));
+}
--- lib/app/quiz/widgets/home.dart
+++ lib/app/quiz/widgets/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_training/app/quiz/widgets/custom_text.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -12,8 +13,40 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    var totalWidht = MediaQuery
+        .of(context)
+        .size
+        .width;
+    var totalheight = MediaQuery
+        .of(context)
+        .size
+        .height;
     return Scaffold(
-      appBar: new AppBar(),
+        appBar: new AppBar(
+          title: new Text(widget.title),
+        ),
+        body: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Card(
+                elevation: 10.0,
+                child: new Container(
+                  height: 400,
+                  width: 400,
+                  child: new Image.asset('assets/deux.jpg',
+                    fit: BoxFit.cover,),
+                ),
+              ),
+              new RaisedButton(
+                color: Colors.green,
+                onPressed: null,
+                child: CustomText(
+                    "Commencer le Quiz", factor: 2, color: Colors.white),)
+
+            ],
+          ),
+        )
     );
   }
 }
