213d07ee0497df4514584c6ce1430090706e321e
Lionel Kouame <lionelkouame@gmail.com>
2020-01-29 00:29:48 +0100

    [#10] -  Quiz - create question class.

---
+++ lib/app/quiz/models/question.dart
@@ -0,0 +1,13 @@
+class Question {
+  String question;
+
+  bool answer;
+
+  String explication;
+
+  Question(String question, bool answer, String explication) {
+    this.question = question;
+    this.answer = answer;
+    this.explication = explication;
+  }
+}
--- lib/app/quiz/widgets/page_quiz.dart
+++ lib/app/quiz/widgets/page_quiz.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'custom_text.dart';
+import '../models/question.dart';
+
 
 class PageQuiz extends StatefulWidget {
   @override
@@ -7,6 +9,26 @@ class PageQuiz extends StatefulWidget {
 }
 
 class _PageQuizState extends State<PageQuiz> {
+
+  Question question;
+
+  List<Question> questions = [
+    new Question("yo ?", true, "Je suis en train de faire qui 1 "),
+    new Question("yo ?", false, "Je suis en train de faire qui 2 "),
+    new Question("yo ?", true, "Je suis en train de faire qui 3 "),
+    new Question("yo ?", false, "Je suis en train de faire qui 4 "),
+    new Question("yo ?", false, "Je suis en train de faire qui 5 "),
+  ];
+
+  int index = 0;
+  int score = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    question = questions[0];
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
