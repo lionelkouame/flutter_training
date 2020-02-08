89e26db5c53958dfc859338c8aecfa9dc0206602
Lionel Kouame <lionelkouame@gmail.com>
2020-02-08 10:09:19 +0100

    [#12] - Interactives widget - button radion.

--- lib/app/interactive_control/radio_training.dart
+++ lib/app/interactive_control/radio_training.dart
@@ -7,6 +7,33 @@ class RadioTraining extends StatefulWidget {
 }
 
 class _RadioTrainingState extends State<RadioTraining> {
+
+  int itemSelection;
+
+  List<Widget> radios() {
+    List<Widget> l = [];
+    for (int x = 0; x < 5; x++) {
+      Row row = Row(
+        children: <Widget>[
+          CustomText("Le choix numero ${x + 1}",
+              color: (x == itemSelection) ? Colors.green : Colors.red),
+          Radio(
+            value: x,
+            groupValue: itemSelection,
+            onChanged: (int i) {
+              setState(() {
+                itemSelection = i;
+              });
+            },
+          )
+        ],
+      );
+      l.add(row);
+    }
+
+    return l;
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -16,7 +43,7 @@ class _RadioTrainingState extends State<RadioTraining> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[],
+          children: radios(),
         ),
       ),
     );
